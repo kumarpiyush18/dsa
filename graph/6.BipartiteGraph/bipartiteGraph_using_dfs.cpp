81a1949231bae9cb25447497503f175772e99e59
Kumar Piyush <krpiyush3@gmail.com>
2022-07-21 15:58:07 +0530

    dfsImp

---
+++ graph/6.BipartiteGraph/bipartiteGraph_using_dfs.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+bool dfsHelper(int s,vector<int>adj,vector<int>&color){
+    if(color[s]==-1)
+    color[s]=1;
+
+    for(auto x : adj[s]){
+        if(color[x]==-1){
+            color[x]=1- color[s];
+            if(!dfsHelper(x,adj,color)){
+                return false;
+            }
+            else if (color[x]==color[s]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool isBipartite(int V,vector<int>adj[]){
+
+    vector<int>color(V+1,-1);
+
+    for(int i=0;i<V;i++){
+        if(!dfsHelper(i,adj,color)){
+            return false;
+        }
+    }
+    return true;
+}
